702085831806336fa4dbacb5f814314f2f5c7574
Neotwalker <and1marat9@gmail.com>
2022-12-16 16:59:38 +0500

    add

--- js/common.js
+++ js/common.js
@@ -1,10 +1,10 @@
 $(function() {
 
-	// $('.marquee').marquee({
-	// 	duration: 24000,
-	// 	startVisible: true,
-	// 	duplicated: true
-	// });
+	$('.marquee').marquee({
+		duration: 24000,
+		startVisible: true,
+		duplicated: true
+	});
 
 	$('.down').click(function(){
 		let href = $(this).attr('href');
@@ -19,7 +19,34 @@ $(function() {
 		return false;
 	});
 
+	let menuBtn = $('.burger');
+	let menu = $('.menu');
+	menuBtn.click( function(){
+		menu.slideToggle('medium').toggleClass('active');
+		$('body').toggleClass("overlay");
+		$('.burger--icon').toggleClass('open');
+	})
+	// $('.burger').click(function () {
+	// 	$('.menu').slideToggle('medium');
+	// });
+
 });
+
+// let element = document.querySelector('.slider');
+// let cnt = 0
+
+// document.addEventListener('wheel', function(event){
+// 	event = event || window.event;
+// 	let y = event.deltaY || event.detail || event.wheelDelta, val=0.1, min = 0, max=0;
+// 	if(y > 0){
+// 			cnt = cnt - 20;
+// 			element.style.transform = 'translate(' + cnt + '%)'
+// 	}
+// 	if(y < 0){
+// 			cnt = cnt + 20;
+// 			element.style.transform = 'translate(' + cnt + '%)'
+// 	}
+// });
 
 document.addEventListener('DOMContentLoaded', () => {
 	const section = document.querySelector('.slider');
@@ -33,4 +60,4 @@ document.addEventListener('DOMContentLoaded', () => {
 			section.scrollLeft  += e.deltaY 
 		}
 	})
-})
+});
