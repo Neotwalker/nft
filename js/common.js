061a32a0d987bcaa9f07086bc35fd4340e79f04f
Neotwalker <and1marat9@gmail.com>
2022-12-23 18:13:01 +0500

    add

--- js/common.js
+++ js/common.js
@@ -1,13 +1,5 @@
 $(function() {
 
-	$('.marquee').marquee({
-		duration: 32000,
-		allowCss3Support: true,
-		css3easing: 'linear',
-		startVisible: true,
-		duplicated: true
-	});
-
 	$('.down').click(function(){
 		let href = $(this).attr('href');
 
@@ -28,27 +20,8 @@ $(function() {
 		$('body').toggleClass("overlay");
 		$('.burger--icon').toggleClass('open');
 	})
-	// $('.burger').click(function () {
-	// 	$('.menu').slideToggle('medium');
-	// });
 
 });
-
-// let element = document.querySelector('.slider');
-// let cnt = 0
-
-// document.addEventListener('wheel', function(event){
-// 	event = event || window.event;
-// 	let y = event.deltaY || event.detail || event.wheelDelta, val=0.1, min = 0, max=0;
-// 	if(y > 0){
-// 			cnt = cnt - 20;
-// 			element.style.transform = 'translate(' + cnt + '%)'
-// 	}
-// 	if(y < 0){
-// 			cnt = cnt + 20;
-// 			element.style.transform = 'translate(' + cnt + '%)'
-// 	}
-// });
 
 document.addEventListener('DOMContentLoaded', () => {
 	const section = document.querySelector('.slider');
